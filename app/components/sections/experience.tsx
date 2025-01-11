61fdfd61aa6fa479a72a84252a384540991b28f1
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-01-11 11:26:56 +0100

    upload a test wobble

--- app/components/sections/experience.tsx
+++ app/components/sections/experience.tsx
@@ -1,25 +1,118 @@
-import { OrbitControls } from "@react-three/drei";
-import { useControls, button } from "leva";
-import { Perf } from "r3f-perf";
+import {Environment, OrbitControls, PerspectiveCamera, useHelper} from "@react-three/drei";
+import {useControls} from "leva";
+import {Perf} from "r3f-perf";
+import * as THREE from "three";
+import wobbleVertexShader from "../../shaders/wobble/vertex.glsl";
+import wobbleFragmentShader from "../../shaders/wobble/fragment.glsl";
+import {mergeVertices} from "three/addons/utils/BufferGeometryUtils.js";
+import CustomShaderMaterial from "three-custom-shader-material";
+import {useEffect, useRef} from "react";
+import {CameraHelper, DirectionalLight, DirectionalLightHelper, Mesh} from "three";
+import {useFrame} from "@react-three/fiber";
 
 export default function Experience() {
-  const { perfVisible } = useControls("debug", {
-    perfVisible: false,
-  });
-  return (
-    <>
-      {perfVisible && <Perf position="top-left" />}
+    const {perfVisible} = useControls("debug", {
+        perfVisible: false,
+    });
 
-      <OrbitControls makeDefault />
+    const uniforms = {
+        uTime: new THREE.Uniform(0),
+        uPositionFrequency: new THREE.Uniform(0.5),
+        uTimeFrequency: new THREE.Uniform(0.4),
+        uStrength: new THREE.Uniform(0.3),
+        uWarpPositionFrequency: new THREE.Uniform(0.38),
+        uWarpTimeFrequency: new THREE.Uniform(0.12),
+        uWarpStrength: new THREE.Uniform(1.7),
+        uColorA: new THREE.Uniform(new THREE.Color("#0000ff")),
+        uColorB: new THREE.Uniform(new THREE.Color("#ff0000")),
+    };
 
-      <directionalLight position={[1, 2, 3]} intensity={4.5} />
-      <ambientLight intensity={1.5} />
+    const meshRef = useRef<Mesh>(null);
+    useEffect(() => {
+        let geometry = meshRef.current?.geometry;
 
-      <color args={["#bdedfc"]} attach="background" />
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial color="mediumpurple" />
-      </mesh>
-    </>
-  );
+        if (geometry) {
+            geometry = mergeVertices(geometry);
+            geometry.computeTangents();
+        }
+    }, []);
+
+    useFrame((state) => {
+        const elapsedTime = state.clock.getElapsedTime();
+        // uniforms.uTime.value = elapsedTime;
+
+        if(meshRef.current) {
+            const material = meshRef.current.material as THREE.ShaderMaterial;
+            if (material.uniforms) {
+                material.uniforms.uTime.value = elapsedTime;
+            }
+        }
+    });
+
+    const lightRef = useRef<DirectionalLight>(null);
+    const cameraRef = useRef<THREE.PerspectiveCamera>(null);
+    useHelper(cameraRef as React.MutableRefObject<THREE.Object3D>, CameraHelper);
+    useHelper(lightRef as React.MutableRefObject<DirectionalLight>, DirectionalLightHelper, 1, "red");
+
+    return (
+        <>
+            {perfVisible && <Perf position="top-left"/>}
+
+            <OrbitControls makeDefault/>
+
+            <directionalLight
+                ref={lightRef}
+                color={'#ffffff'}
+                position={[0.25, 2, 2.25]}
+                intensity={3}
+                castShadow
+                shadow-mapSize-width={1024}
+                shadow-mapSize-height={1024}
+                shadow-camera-far={15}
+                shadow-normalBias={0.05}
+            />
+
+            <PerspectiveCamera ref={cameraRef} near={0.1} far={200} position={[0, 0, 15]} fov={45}></PerspectiveCamera>
+            <Environment
+                background
+                files="./urban_alley_01_1k.hdr"
+            >
+            </Environment>
+
+            <color args={["#bdedfc"]} attach="background"/>
+            <mesh ref={meshRef} scale={0.5} castShadow receiveShadow>
+                <icosahedronGeometry args={[2.5, 50]}/>
+                <CustomShaderMaterial
+                    baseMaterial={THREE.MeshPhysicalMaterial}
+                    vertexShader={wobbleVertexShader}
+                    fragmentShader={wobbleFragmentShader}
+                    uniforms={uniforms}
+                    wireframe={false}
+                    metalness={0}
+                    roughness={0.5}
+                    color={"#ffffff"}
+                    transmission={0}
+                    ior={1.5}
+                    thickness={1.5}
+                    transparent={true}
+                />
+                <CustomShaderMaterial
+                    baseMaterial={THREE.MeshDepthMaterial}
+                    vertexShader={wobbleVertexShader}
+                    uniforms={uniforms}
+                    depthPacking={THREE.RGBADepthPacking}
+                    attach="depthMaterial"
+                />
+            </mesh>
+
+            <mesh
+                rotation={[0, Math.PI, 0]}
+                position={[0, -5, -5]}
+                receiveShadow
+            >
+                <planeGeometry args={[15, 15, 15]}/>
+                <meshStandardMaterial side={THREE.DoubleSide}/>
+            </mesh>
+        </>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
 "use client";
-import { Canvas } from "@react-three/fiber";
+import {Canvas} from "@react-three/fiber";
 import Experience from "./components/sections/experience";
 
 export default function Home() {
-  return (
-    <Canvas
-      shadows
-      camera={{
-        fov: 45,
-        near: 0.1,
-        far: 200,
-        position: [0, 0, 5],
-      }}
-    >
-      <Experience />
-    </Canvas>
-  );
+    return (
+        <Canvas shadows>
+            <Experience/>
+        </Canvas>
+    );
 }
