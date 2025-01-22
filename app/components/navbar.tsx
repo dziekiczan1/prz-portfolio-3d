11ef2acc6c09c43d8e4feadcb025d6f48f803539
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-01-22 20:03:11 +0100

    add navigation data

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import {HoverBorderGradient} from '@/components/ui/button';
 import {useEffect, useState} from "react";
+import {navigationMenuData} from "@/constants/navigation";
 
 export default function Navbar() {
     const [activeSection, setActiveSection] = useState('');
@@ -41,7 +42,7 @@ export default function Navbar() {
                 className="flex items-center justify-center font-bold gap-4"
             >
                 <div
-                    className="flex items-center justify-center border border-fuchsia-800 border-2 w-10 h-10 rounded-lg">
+                    className="flex items-center justify-center border-2 border-fuchsia-800 w-10 h-10 rounded-lg">
                     <Image
                         src={`/erzet.webp`}
                         width={32}
@@ -53,42 +54,21 @@ export default function Navbar() {
                 <p className="uppercase font-orbitron text-sm">Piotr<br/> Rzadkowolski</p>
             </Link>
             <nav className="flex items-center gap-8">
-                <Link
-                    href="#about"
-                    className="relative group"
-                >
-                    About
-                    <span
-                        className={`absolute left-0 bottom-0 h-0.5 bg-green-500 transition-all duration-500 transform origin-center ${
-                            activeSection === 'about' ? 'scale-x-100' : 'scale-x-0'
-                        }`}
-                        style={{ width: '100%' }}
-                    />
-                </Link>
-                <Link
-                    href="#projects"
-                    className="relative group"
-                >
-                    Projects
-                    <span
-                        className={`absolute left-0 bottom-0 h-0.5 bg-green-500 transition-all duration-500 transform origin-center ${
-                            activeSection === 'projects' ? 'scale-x-100' : 'scale-x-0'
-                        }`}
-                        style={{ width: '100%' }}
-                    />
-                </Link>
-                <Link
-                    href="#resume"
-                    className="relative group"
-                >
-                    Resume
-                    <span
-                        className={`absolute left-0 bottom-0 h-0.5 bg-green-500 transition-all duration-500 transform origin-center ${
-                            activeSection === 'resume' ? 'scale-x-100' : 'scale-x-0'
-                        }`}
-                        style={{ width: '100%' }}
-                    />
-                </Link>
+                {navigationMenuData.map((item) => (
+                    <Link
+                        key={item.link}
+                        href={item.link}
+                        className="relative group"
+                    >
+                        {item.sectionName}
+                        <span
+                            className={`absolute left-0 bottom-0 h-0.5 bg-fuchsia-700 transition-all duration-500 transform origin-center ${
+                                activeSection === item.name ? 'scale-x-100' : 'scale-x-0'
+                            }`}
+                            style={{width: '100%'}}
+                        />
+                    </Link>
+                ))}
                 <Link href="#contact">
                     <HoverBorderGradient
                         containerClassName="rounded-full"
---
+++ constants/navigation.ts
@@ -0,0 +1,21 @@
+export const navigationMenuData: {
+    name: string,
+    sectionName: string,
+    link: string,
+}[] = [
+    {
+        name: 'about',
+        sectionName: 'About',
+        link: '#about',
+    },
+    {
+        name: 'projects',
+        sectionName: 'Projects',
+        link: '#projects',
+    },
+    {
+        name: 'resume',
+        sectionName: 'Resume',
+        link: '#resume',
+    },
+];
